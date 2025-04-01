70fdda5f3b40f8dbe016dcaf2dcce7cdf0982393
Ivan <kireev.ivan.8@gmail.com>
2025-04-01 17:30:33 +0700

    feat: datagrid

--- TestDB/Form1.Designer.cs
+++ TestDB/Form1.Designer.cs
@@ -28,13 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridViewBooks = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewBooks)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewBooks
+            // 
+            this.dataGridViewBooks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewBooks.Location = new System.Drawing.Point(13, 13);
+            this.dataGridViewBooks.Name = "dataGridViewBooks";
+            this.dataGridViewBooks.RowHeadersWidth = 102;
+            this.dataGridViewBooks.RowTemplate.Height = 40;
+            this.dataGridViewBooks.Size = new System.Drawing.Size(1239, 761);
+            this.dataGridViewBooks.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1264, 786);
+            this.Controls.Add(this.dataGridViewBooks);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewBooks)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewBooks;
     }
 }
 
--- TestDB/Form1.cs
+++ TestDB/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TestDB.Models;
 
 namespace TestDB
 {
@@ -15,6 +16,61 @@ namespace TestDB
         public Form1()
         {
             InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadBooksData();
+        }
+        private void LoadBooksData()
+        {
+            try
+            {
+                // 1. Получите контекст базы данных.
+                using (var db = LibraryDBEntities.GetContext())
+                {
+                    // 2. Получите данные из таблицы "Книги".
+                    var books = db.Books
+                     .Select(b => new
+                     {
+                         b.BookID,
+                         b.Title,
+                         AuthorName = b.Authors.FullName, // обращаемся к полю FullName из таблицы Authors
+                         b.PublicationYear,
+                         b.Price
+                     })
+                     .ToList();
+                    // 3. Привяжите данные к DataGridView.
+                    dataGridViewBooks.DataSource = books;
+                    // 4. Настройка DataGridView
+                    FormatDataGridView();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при загрузке данных: {ex.Message}");
+            }
+        }
+        private void FormatDataGridView()
+        {
+            // Задаем заголовки столбцов
+            dataGridViewBooks.Columns["BookID"].HeaderText = "ID Книги";
+            dataGridViewBooks.Columns["Title"].HeaderText = "Название";
+            dataGridViewBooks.Columns["AuthorName"].HeaderText = "Автор";
+            dataGridViewBooks.Columns["PublicationYear"].HeaderText = "Год издания";
+            dataGridViewBooks.Columns["Price"].HeaderText = "Цена";
+            // Скрываем столбец AuthorID, если он больше не нужен
+            if (dataGridViewBooks.Columns.Contains("AuthorID"))
+            {
+                dataGridViewBooks.Columns["AuthorID"].Visible = false;
+            }
+            // Форматирование столбца Price для отображения как валюты
+            dataGridViewBooks.Columns["Price"].DefaultCellStyle.Format = "c";
+            // Обработка Nullable<int> для PublicationYear
+            dataGridViewBooks.Columns["PublicationYear"].DefaultCellStyle.NullValue = "";
+            // Запрет редактирования данных
+            dataGridViewBooks.ReadOnly = true;
+            // Автоматическая подгонка ширины столбцов по содержимому
+            dataGridViewBooks.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells;
         }
     }
 }
